d968ac7a972300a1f3b2839aba592e317d2dda52
rajdosi <rajdosi10@gmail.com>
2016-03-01 11:34:29 +0530

    broadcast method and initial state defined

--- NetworkSim/src/NetworkSimBase/Project.java
+++ NetworkSim/src/NetworkSimBase/Project.java
@@ -15,30 +15,32 @@ public class Project{
     
 	private static int count = 0;
     private static Map<Integer, NetworkNode> GlobalDataStructure = new HashMap<>();
+    private static int distanceChecked = 2;
+    private static int[][] networkMap = new int[400][400];
     
-//    public void broadcast(int num)
-//    {
-//       int i,j=0;
-//       for(i=0;;i++)
-//       {
-//           commonVariables.xCordinate[j] = commonVariables.xCordinate[j] + i;
-//           if(commonVariables.xCordinate[j] == commonVariables.xCordinate[j+1])
-//           {
-//               networkNode.localXCoordinate = commonVariables.xCordinate[i];
-//               break;
-//       //System.out.println("The nieghbours of node "+num+" are:");
-//       } 
-//       for(i=0;;i++)
-//            }
-//       {
-//           commonVariables.yCordinate[j] = commonVariables.yCordinate[j] + i;
-//           if(commonVariables.yCordinate[j] == commonVariables.yCordinate[j+1])
-//           {
-//               break;
-//           }
-//       //System.out.println("The nieghbours of node "+num+" are:");
-//       } 
-//    } 
+    public static void broadcast(int xCoordinate, int yCoordinate, NetworkNode currentNetworkNode) {
+    	count++;
+    	if (xCoordinate == -1 || yCoordinate == -1 || xCoordinate == 400 || yCoordinate == 400 || count > distanceChecked )
+    		return;
+    	if (networkMap[xCoordinate][yCoordinate] != -1){ 		
+    		currentNetworkNode.neighborCount++;
+    		currentNetworkNode.neighbourList.add(networkMap[xCoordinate][yCoordinate]);
+    	}
+    	broadcast(xCoordinate-1,yCoordinate,currentNetworkNode);
+    	broadcast(xCoordinate-1,yCoordinate-1,currentNetworkNode);
+    	broadcast(xCoordinate,yCoordinate-1,currentNetworkNode);
+    	broadcast(xCoordinate+1,yCoordinate-1,currentNetworkNode);
+    	broadcast(xCoordinate+1,yCoordinate,currentNetworkNode);
+    	broadcast(xCoordinate+1,yCoordinate+1,currentNetworkNode);
+    	broadcast(xCoordinate,yCoordinate+1,currentNetworkNode);
+    	broadcast(xCoordinate-1,yCoordinate+1,currentNetworkNode);
+    }
+    
+    public static void setNetworkMap() {
+    	for (int i=0;i<400;i++)
+    		for (int j=0;j<400;j++)
+    			networkMap[i][j] = -1;
+    }
     
     public static void init() throws IOException{
     	FileReader in = new FileReader("/home/wazza/workplace/NetworkSimulator/NetworkSim/src/initFile.txt");
@@ -58,6 +60,8 @@ public class Project{
                 temporaryNode.localXCoordinate = Integer.parseInt(line.substring(del+1,comma));
                 temporaryNode.localYCoordinate = Integer.parseInt(line.substring(comma+1,at));
                 
+                networkMap[temporaryNode.localXCoordinate][temporaryNode.localYCoordinate] = temporaryNode.nodeID;
+                
                 GlobalDataStructure.put(temporaryNode.nodeID,temporaryNode);
                 count++;
         	} 
@@ -69,10 +73,12 @@ public class Project{
     
     public static void main(String[] args) throws IOException {
     	init();
+    	setNetworkMap();
     	
-    	for (int i=1; i<GlobalDataStructure.size() + 1; i++) {
-    		System.out.println(GlobalDataStructure.get(i).localXCoordinate);		
-    	}
+    	for (int i = 0; i < GlobalDataStructure.size(); i++ ) {
+    		count=0;
+    		broadcast(GlobalDataStructure.get(i+1).localXCoordinate, GlobalDataStructure.get(i+1).localYCoordinate, GlobalDataStructure.get(i+1));	
+    	}		
     }
 }
 
